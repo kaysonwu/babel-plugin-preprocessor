9c2a7d76e8f3810a1485ea392825f779d625b0be
kaysonwu <772663139@qq.com>
2020-04-18 11:28:37 +0800

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
     '@babel/preset-typescript',
-    ['@babel/preset-env', { targets: { node: true } }],
+    ['@babel/preset-env', { targets: { node: 8 } }],
   ],
 };
